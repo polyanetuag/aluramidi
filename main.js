822e028bdb2e17fdfd1a8276a60a112fd04784ca
Polyane Tuag <pollyportugues@hotmail.com>
2023-02-05 09:58:34 -0300

    fix: testos dinamicos

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ const listaDeTeclas = document.querySelectorAll('.tecla')
 let contador = 0
 
 while(contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+
     listaDeTeclas[contador].onclick = function() {
-        playSound(`#${this.id}`)
+        playSound(idAudio)
     }
     contador++
 

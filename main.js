82b5a9eb02569e47888c1db86e0a809b3d61dff4
Polyane Tuag <pollyportugues@hotmail.com>
2023-02-06 19:15:03 -0300

    feat: add condição para o teclado

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 function playSound(idSound) {
-    document.querySelector(idSound).play()
+  document.querySelector(idSound).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
+  listaDeTeclas[contador].onclick = function () {
+    playSound(idAudio);
+  };
 
-for(let contador = 0; contador < listaDeTeclas.length; contador++) {
-    const tecla = listaDeTeclas[contador]
-    const instrumento = tecla.classList[1]
-    const idAudio = `#som_${instrumento}`
-
-    listaDeTeclas[contador].onclick = function() {
-        playSound(idAudio)
+  tecla.onkeydown = function (event) {
+    if (event.code === "Space" || event.code === "Enter") {
+      tecla.classList.add("ativa");
     }
+  };
 
-    tecla.onkeydown = function() {
-        tecla.classList.add('ativa')
-    }
-
-    tecla.onkeyup = function() {
-        tecla.classList.remove('ativa')
-    }
-
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
